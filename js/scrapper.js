5abbdd816d934de9edc768c55947a3b3c2655fe0
Johnson Liang <johnsonliang7@gmail.com>
2012-05-07 02:23:21 +0800

    scrapping

--- js/scrapper.js
+++ js/scrapper.js
@@ -1,1 +1,7 @@
-console.log('scrapper~~');
+(function(doc){
+  var stories = doc.querySelectorAll('.uiStreamStory'), i;
+  for(i=0; i<stories.length; i+=1){
+    console.log('STORY:',stories[i],stories[i].dataset);
+    // find input[name=feedback_params]
+  }
+}(document));
