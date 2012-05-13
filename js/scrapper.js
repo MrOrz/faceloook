188ebdad61b6d3cd0c8d29e206e53318e34e1f47
MrOrz <johnsonliang7@gmail.com>
2012-05-14 01:11:02 +0800

    Uses timestamp links to get ID.  MutationObserver added to  watch DOM insertion.

--- js/scrapper.js
+++ js/scrapper.js
@@ -1,7 +1,81 @@
-(function(doc){
-  var stories = doc.querySelectorAll('.uiStreamStory'), i;
-  for(i=0; i<stories.length; i+=1){
-    console.log('STORY:',stories[i],stories[i].dataset);
-    // find input[name=feedback_params]
+/*global WebKitMutationObserver */
+
+(function(undefined){
+  "use strict";
+
+  var
+  getStories = function(stories){
+    $(stories).each(function(){
+
+      // Find all time link that contains <abbr>.
+      // Note that not all timestamp <abbr> has the class .timeStamp,
+      // especially in facebook groups.
+      var $link = $(this).find('.uiStreamSource a').has('abbr');
+
+      // It is possible that a story has no uiStreamSource.
+      // (for trivial stories, or stories not viewable(?))
+      // It is also possible that a story has multiple uiStreamSource,
+      // if an object is shared by multiple people.
+      console.log('STORY:',this,'URL: ', $link);
+    });
+  };
+
+  var
+
+  // Ajax-load stories handler
+  newStoryObserver = new WebKitMutationObserver(function(mutations){
+    console.log('New story detected', mutations);
+
+    // call getStories with newly inserted stories
+    getStories(mutations[0].addedNodes);
+  }),
+
+  // Page change handler
+  pageChangeObserver = new WebKitMutationObserver(function(mutations){
+    console.log('pageChangeObserver', mutations);
+
+    var current_ul = $('.uiStreamHomepage').get(0);
+    this.ul = this.ul || null;
+
+    // If the page is surely changed, unregister the old story observer
+    // and reset newStoryObserver to observe the new <ul>
+    if(mutations[0].addedNodes.length > 0 &&
+       this.ul !== current_ul){
+      console.log('UL is re-constructed!');
+      newStoryObserver.disconnect();
+      this.ul = current_ul;
+
+      newStoryObserver.init(
+        $(mutations[0].addedNodes).find('.uiStreamHomepage').get(0));
+    }
+  });
+
+  // Mixin methods into newStoryObserver
+  newStoryObserver.init = function(ul){
+    console.log('Initailizing new-story observer');
+    ul = ul || $('.uiStreamHomepage').get(0);
+    if(ul){
+      this.observe(ul, {childList: true} );
+    }else{
+      console.log('No stream detected on this page.');
+    }
   }
-}(document));
+
+  var testSubtreeObserver = new WebKitMutationObserver(function(mutations){
+    var $nodes = jQuery();
+    $.each(mutations, function(){
+      $nodes = $nodes.add(this.addedNodes);
+    });
+    console.log('Subtree Mutations: ', $nodes);
+  });
+
+  testSubtreeObserver.observe( $('#contentCol').get(0), {subtree: true, childList: true} );
+
+  // Initialize page-change observer
+  pageChangeObserver.observe( $('#contentCol').get(0), {childList: true} );
+
+  // Initialize new-story observer
+  newStoryObserver.init();
+
+  getStories($('.uiStreamStory'));
+}());
