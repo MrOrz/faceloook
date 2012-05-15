09f45a140558b096302931b87b0859d3855624a6
MrOrz <johnsonliang7@gmail.com>
2012-05-15 11:07:44 +0800

    init commit for rails server

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faceloook::Application.config.secret_token = '31bbb4ecf5917f565fd2e1ee99c64f71d7bfcc3ba7dafe0d8d460817a95d17ae435ae1027acca372a275041b1e36e25734feb7249b69121da113396f76417298'
