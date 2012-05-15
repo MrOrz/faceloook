00024584bd8fa0c6157833506b0e28d2876bd6cc
MrOrz <johnsonliang7@gmail.com>
2012-05-15 10:49:35 +0800

    Popup now shows database items

---
+++ js/popup.js
@@ -0,0 +1,42 @@
+(function(undefined){
+  "use strict";
+
+  var
+  $access_token = $('#access_token'),
+  $clicked = $('.clicked tbody'),
+  $not_clicked = $('.not_clicked tbody'),
+  trMaker = function(row){
+    var
+    td1 = $('<td><a href="javascript:;">' + row.fbid + '</a></td>'),
+    td2 = $('<td>' + row.updated_at + '</td>');
+    return $('<tr></tr>').append(td1).append(td2);
+  };
+
+  $.db("SELECT * FROM entry WHERE clicked=1 ORDER BY updated_at DESC;",
+    [], function(tx, result){
+      var rows = result.rows, i;
+      for(i=0; i<rows.length; i+=1){
+        trMaker(rows.item(i)).appendTo($clicked);
+      }
+    }
+  );
+
+  $.db("SELECT * FROM entry WHERE clicked=0 ORDER BY updated_at DESC;",
+    [], function(tx, result){
+      var rows = result.rows, i;
+      for(i=0; i<rows.length; i+=1){
+        trMaker(rows.item(i)).appendTo($not_clicked);
+      }
+    }
+  );
+
+  $('table').on('click', 'a', function(e){
+    e.preventDefault();
+    var fbid = $(this).text();
+    $.getJSON('https://graph.facebook.com/' + fbid, {
+      access_token: $access_token.val()
+    }, function(data){
+      alert(data.from.name + ': ' + data.message);
+    })
+  });
+}());
