a6b2e146f08d8f2c1a1ea239e672fe2ce42f818e
MrOrz <johnsonliang7@gmail.com>
2012-06-14 13:23:40 +0800

    recursive cas

--- app/controllers/cas_controller.rb
+++ app/controllers/cas_controller.rb
@@ -1,7 +1,28 @@
 class CasController < ApplicationController
   require 'RMMSegProxy'
 
+  # /q?t[ID]=ARTICLE&t[ID2]=ARTICLE2&t[ID3][message]=lalaa
   def query
-    render :text => seg(params[:term]).join(' ')
+    terms = params[:t] || {}
+    render :json => recursive(params[:t])
   end
+
+private
+  def recursive obj
+    ret = {}
+    unless obj.nil?
+      obj.each do |key, val|
+        if val.class != String
+          ret[key] = recursive val
+        else
+          ret[key] = do_segmentation val
+        end
+      end
+    end
+    return ret
+  end
+  def do_segmentation term
+    seg(term).join(' ')
+  end
+
 end
