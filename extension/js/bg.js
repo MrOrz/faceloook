ee480f0d6172087b952092b508e03475a52eaddd
MrOrz <johnsonliang7@gmail.com>
2012-06-20 20:04:15 +0800

    Move processData to process.js. Global method "GET" is added

--- extension/js/bg.js
+++ extension/js/bg.js
@@ -1,9 +1,7 @@
-/*global chrome, FB, DB, _, CAS */
+/*global chrome, FB, DB, _, GET */
 
 (function(chrome, undefined){
   "use strict";
-
-  var BATCH_COUNT = 5;
 
   // Chrome extension listener
   chrome.extension.onRequest.addListener(function(request){
@@ -23,110 +21,7 @@
     }
   });
 
-  // Processing untrained data
-  var
-
-  // Batch processing the data in database rows.
-  // First it asks FB to get the items needed,
-  // then it consults CAS to get tokenized text features.
-  batch = function(rows){
-    var
-      dfd = $.Deferred(),
-      fbids = _(rows).pluck('fbid'),
-      rowData = {}; // id -> database row data
-
-    _(rows).each(function(item){
-      rowData[item.fbid] = item;
-    });
-
-    // Start querying FB
-    FB.get('', {ids: fbids.join(',')}, function(data){
-      var items = {}, itemsToTokenize = {};
-
-      if(data.length === 0){
-        dfd.resolve({});
-        return;
-      }
-
-      // Split item properties into 2 categories:
-      // those to be tokenized, and those that are not.
-      $.each(data, function(){
-        items[this.id] = {
-          id : this.id,
-          groupId : '',
-          from : this.from.id,
-          updated : this.updated_time || this.created_time
-        };
-
-        itemsToTokenize[this.id] = {
-          message : this.message || "",
-          link : this.link || "",
-          linkName : this.name || "",
-          linkDesct : this.description || ""
-        };
-
-        // Group message detection
-        var tokens = this.id.split('_');
-        if(tokens.length === 2){
-          items[this.id].groupId = tokens[0];
-          items[this.id].id = tokens[1];
-        }
-      });
-
-      CAS(itemsToTokenize, function(tokenized){
-        // merge items with tokenized items
-        var ret = $.extend(true, {}, items, tokenized);
-
-        // mixin rowData only when data is returned from facebook
-        _(ret).each(function(value, key){
-          ret[key].rowData = rowData[value.id];
-        });
-        dfd.resolve(ret);
-      }).fail(function(){
-        // CAS query fail, reject.
-        dfd.reject(arguments);
-      });
-    }, function(){
-      // FB query fail, reject.
-      dfd.reject(arguments);
-    });
-
-    // Return dfd.
-    return dfd.promise();
-  },
-
-  // Fetch data from database, FB and CAS, and calls callback
-  // whenever any data is ready.
-  processData = function(callback){
-    DB.getUntrained(function(tx, result){
-      var rows = result.rows, i = 0;
-
-      console.log('Processing ', rows.length, ' items...');
-
-      // Proceed processing BATCH_COUNT items.
-      var proceed = function(){
-        var j = 0, buffer = [];
-        for(; i < rows.length && j<BATCH_COUNT; i+=1, j+=1){
-          buffer.push(rows.item(i));
-        }
-        if(buffer.length){
-          // Process the items in buffer. When the batch process is done,
-          // invoke callback and proceed to the next BATCH_COUNT items.
-          batch(buffer).done(function(processedData){
-            console.log('First ', i, ' items are processed.', processedData);
-            _(callback).defer(processedData); // heavy-lifting
-          }).fail(function(){
-            console.error('Batch processing failed: ', arguments);
-          }).always(proceed);
-        }
-      };
-
-      // Start processing.
-      proceed();
-    });
-  };
-
-  processData(function(data){
+  GET(DB.getUntrained, function(data){
     /*
       data = {
         131051573672766_242421059202483: {
