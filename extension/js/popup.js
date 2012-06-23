2168a0b6ca8ce6472887cfdc2860023dc02de892
MrOrz <johnsonliang7@gmail.com>
2012-06-24 04:12:16 +0800

    不知為啥有些status append不上去.....

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,59 +1,103 @@
-/*global DB, BAYES, FB */
+/*global DB, BAYES, FB, GET, _ */
 (function(undefined){
   "use strict";
-
-  var
-  $clicked = $('.clicked tbody'),
-  $not_clicked = $('.not_clicked tbody'),
-  trMaker = function(row){
-    var
-    td1 = $('<td><a href="javascript:;">' + row.fbid + '</a></td>'),
-    td2 = $('<td>' + row.updated_at + '</td>');
-    return $('<tr></tr>').append(td1).append(td2);
-  };
-
-  DB("SELECT * FROM entry WHERE clicked=1 ORDER BY updated_at DESC;",
-    [], function(tx, result){
-      var rows = result.rows, i;
-      for(i=0; i<rows.length; i+=1){
-        trMaker(rows.item(i)).appendTo($clicked);
-      }
-    }
-  );
-
-  DB("SELECT * FROM entry WHERE clicked=0 ORDER BY updated_at DESC;",
-    [], function(tx, result){
-      var rows = result.rows, i;
-      for(i=0; i<rows.length; i+=1){
-        trMaker(rows.item(i)).appendTo($not_clicked);
-      }
-    }
-  );
-
-  // $('table').on('click', 'a', function(e){
-  //   e.preventDefault();
-  //   var fbid = $(this).text();
-  //   $.getJSON('https://graph.facebook.com/' + fbid, {
-  //     access_token: $access_token.val()
-  //   }, function(data){
-  //     alert(data.from.name + ': ' + data.message);
-  //   })
-  // });
 
   var lists = {
     'TYPEstatus': $('.status'),
     'TYPEvideo': $('.video'),
     'TYPEphoto': $('.photo'),
     'TYPElink': $('.link'),
-    'TYPEcheckin': $('.checkin')
+    'TYPEcheckin': $('.checkin'),
+    'TYPEnormal': $('.status')
   };
 
-  var API_URL = "https://graph.facebook.com/";
+
+  var
+  API_URL = "https://graph.facebook.com/",
+  suggestions = [];
 
   /*GET(src, function(){})\
   src 可以是 facebook id 的陣列
   function(data){ } 拿到的會是斷詞過後的結果*/
   //FB.get('me/feed', {limit:10}, function(data){...})
+  var app2div = function(sortToken){
+    $.each(sortToken,function(k,v){
+      console.log('v:',v);
+      var postId = FB.ID(v.id);
+      console.log('postId:',postId);
+      if(v.type === 'TYPEphoto' || v.type === 'TYPEvideo'){
+        // make text information
+        var info = "" ;
+        if(v.originMsg !== "" ){
+          info = info + '<span class="pmsg">' + v.originMsg + '</span></br>' ;
+        }
+        if(v.originCap !== "" ){
+          info = info + '<span class="pmsg cap">' + v.originCap + '</span></br>' ;
+        }
+        if(v.originLinkDesct !== "" ){
+          info = info + '<span class="pmsg des">' + v.originLinkDesct + '</span></br>' ;
+        }
+        console.log('info:',info);
+        // append
+        var now = new Date();
+        var updateTime = new Date()
+        var age = (now - v.updated)/60000;
+        console.log('age:',age);
+        lists[v.type].append('<div class="onePhoto">' +
+          '<a href="' + v.originLink + '">' +
+            '<img class="p" src="' + v.picture + '">' +
+          '</a>' +
+          '<div class="info">' +
+            '<a href="https://www.facebook.com/' + v.from + '/posts/' + postId  + '" target="_blank">' +
+              '<div class="pmsg">' + info + '</div>' +
+            '</a>' +
+            '<div>' +
+              '<a href="https://www.facebook.com/' + v.from  + '">' +
+                '<span class="pmsg u">' + v.name + '</span>' +
+              '</a>' +
+              '<span class="pmsg time">' + age + ' mins</span></br>' +
+            '</div>' +
+          '</div>' +
+        '</div>');
+      }
+      else if(v.type === 'TYPEcheckin'){
+        lists[v.type].append('<div class="oneCheck">' +
+          '</div>');
+      }
+      else if(v.type === 'TYPEstatus' || 1 ){
+        console.log('v.type === status');
+        console.log(lists[v.type]);
+        lists[v.type].append('<div class="oneStatus" > ' +
+          '<img class="u" src="' + API_URL + v.from + '/picture">' +
+          '<div class="text">' +
+            '<a href="https://www.facebook.com/' + v.from + '/posts/' + postId  + '" target="_blank">' +
+              '<span class="name">' + v.name + '</span>' + '<br>' +
+              '<span class="msg">' + v.originMsg + '</span>' +
+            '</a>' +
+          '</div>' +
+        '</div>'
+        );
+        console.log('v.type === status doneeeeee');
+      }
+    });
+  };
+
+  GET(DB.getRecentlySeen, function(data){
+    // data is the same as bg.js
+    console.log('GET, data: ',data);
+    //sorted
+    $.each(data,function(k,v){
+      v.prob = BAYES.getProb(v);
+      suggestions.push(v);
+    });
+    console.log('after each, data: ',data);
+  },function(){
+    console.log('suggestions: ', suggestions);
+    var sortToken = _.sortBy(suggestions,function(v){ return -v.prob;});
+    console.log('sortToken: ', sortToken);
+    app2div(sortToken);
+  });
+
   $('#searchform').on('submit',function(e){
     e.preventDefault();
     //clear list
@@ -63,68 +107,11 @@
     // console.log('click search!');
     alert($("#input").val());
     var input = $("#input").val();
-    FB.get('me/home', {q:input,limit:15}, function(data){
-      var itemsToTokenize = {};
-      var item = {};
-      var itemsAfterTokenize = {};
+    FB.get('me/home', {q:input,limit:30}, function(data){
       console.log('FB.get,data: ',data);
-
       BAYES.getCASProb(data.data,function(sortToken){
         console.log(sortToken);
-        $.each(sortToken,function(k,v){
-          console.log('v:',v);
-          var postId = FB.ID(v.id);
-          if(v.type === 'TYPEstatus' ){
-            lists[v.type].append('<div class="oneStatus" > ' +
-              '<img class="u" src="' + API_URL + v.from + '/picture">' +
-              '<div class="text">' +
-                '<a href="https://www.facebook.com/' + v.from + '/posts/' + postId  + '" target="_blank">' +
-                  '<span class="name">' + v.name + '</span>' + '<br>' +
-                  '<span class="msg">' + v.originMsg + '</span>' +
-                '</a>' +
-              '</div>' +
-            '</div>'
-            );
-
-          }
-          else if(v.type === 'TYPEphoto'){
-            // make text information
-            var info = "" ;
-            if(v.originMsg !== "" ){
-              info = info + '<span class="pmsg">' + v.originMsg + '</span></br>' ;
-            }
-            if(v.originCap !== "" ){
-              info = info + '<span class="pmsg cap">' + v.originCap + '</span></br>' ;
-            }
-            if(v.originLinkDesct !== "" ){
-              info = info + '<span class="pmsg des">' + v.originLinkDesct + '</span></br>' ;
-            }
-            console.log('info:',info);
-            //append
-            var now = new Date();
-            var age = (now - v.create_time)/60000;
-            lists[v.type].append('<div class="onePhoto">' +
-              '<a href="' + v.originLink + '">' +
-                '<img class="p" src="' + v.picture + '">' +
-              '</a>' +
-              '<div class="info">' +
-                '<a href="https://www.facebook.com/' + v.from + '/posts/' + postId  + '" target="_blank">' +
-                  '<div class="pmsg">' + info + '</div>' +
-                '</a>' +
-                '<div>' +
-                  '<a href="https://www.facebook.com/' + v.from  + '">' +
-                    '<span class="pmsg u">' + v.name + '</span>' +
-                  '</a>' +
-                  '<span class="pmsg time">' + age + ' mins</span></br>' +
-                '</div>' +
-              '</div>' +
-            '</div>');
-          }
-          else if(v.type === 'TYPEvideo'){
-            lists[v.type].append('<div class="onePhoto">' +
-              '</div>');
-          }
-        });
+        app2div(sortToken);
       });
 
     });
